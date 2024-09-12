8191e74749d728446d2ae3e629f192b513c158fc
Cristian Caloian <cristian.caloian@volvocars.com>
2024-09-12 22:06:48 +0200

    add plugins
    - telescope browser
      - autoclose

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
 return {
+    -- Telescope
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
@@ -41,6 +42,99 @@ return {
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
         end,
     },
+    -- Telescope file browser
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local fb_actions = require("telescope._extensions.file_browser.actions")
+
+            require("telescope").setup({
+                defaults = {
+                    theme = "center",
+                    sorting_strategy = "descending",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "bottom",
+                            preview_width = 0.5,
+                        },
+                    },
+                },
+                extensions = {
+                    file_browser = {
+                        path = vim.loop.cwd(),
+                        cwd = vim.loop.cwd(),
+                        cwd_to_path = false,
+                        grouped = false,
+                        files = true,
+                        add_dirs = true,
+                        depth = 1,
+                        auto_depth = true,
+                        select_buffer = false,
+                        hidden = { file_browser = true, folder_browser = true },
+                        respect_gitignore = vim.fn.executable("fd") == 1,
+                        follow_symlinks = false,
+                        browse_files = require("telescope._extensions.file_browser.finders").browse_files,
+                        browse_folders = require("telescope._extensions.file_browser.finders").browse_folders,
+                        hide_parent_dir = false,
+                        collapse_dirs = false,
+                        prompt_path = false,
+                        quiet = false,
+                        dir_icon = "",
+                        dir_icon_hl = "Default",
+                        display_stat = { date = true, size = true, mode = true },
+                        hijack_netrw = false,
+                        use_fd = true,
+                        git_status = true,
+                        mappings = {
+                            ["i"] = {
+                                ["<A-c>"] = fb_actions.create,
+                                ["<S-CR>"] = fb_actions.create_from_prompt,
+                                ["<A-r>"] = fb_actions.rename,
+                                ["<A-m>"] = fb_actions.move,
+                                ["<A-y>"] = fb_actions.copy,
+                                ["<A-d>"] = fb_actions.remove,
+                                ["<C-o>"] = fb_actions.open,
+                                ["<C-g>"] = fb_actions.goto_parent_dir,
+                                ["<C-e>"] = fb_actions.goto_home_dir,
+                                ["<C-w>"] = fb_actions.goto_cwd,
+                                ["<C-t>"] = fb_actions.change_cwd,
+                                ["<C-f>"] = fb_actions.toggle_browser,
+                                ["<C-h>"] = fb_actions.toggle_hidden,
+                                ["<C-s>"] = fb_actions.toggle_all,
+                                ["<bs>"] = fb_actions.backspace,
+                            },
+                            ["n"] = {
+                                ["c"] = fb_actions.create,
+                                ["r"] = fb_actions.rename,
+                                ["m"] = fb_actions.move,
+                                ["y"] = fb_actions.copy,
+                                ["d"] = fb_actions.remove,
+                                ["o"] = fb_actions.open,
+                                ["g"] = fb_actions.goto_parent_dir,
+                                ["e"] = fb_actions.goto_home_dir,
+                                ["w"] = fb_actions.goto_cwd,
+                                ["t"] = fb_actions.change_cwd,
+                                ["f"] = fb_actions.toggle_browser,
+                                ["h"] = fb_actions.toggle_hidden,
+                                ["s"] = fb_actions.toggle_all,
+                            },
+                        },
+                    },
+                },
+            })
+            require("telescope").load_extension("file_browser")
+
+            -- open file_browser with the path of the current buffer
+            vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+                { desc = "Telescope [F]ile [B]rowser", noremap = true }
+            )
+        end,
+    },
+    -- Telescope UI select
     {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
