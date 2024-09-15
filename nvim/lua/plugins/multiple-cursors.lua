330e0312d0d0db49058c1fa77e76475a6adda830
Cristian Caloian <cristian.caloian@volvocars.com>
2024-09-15 20:58:59 +0200

    add pre and post hooks to multiple cursors

--- nvim/lua/plugins/multiple-cursors.lua
+++ nvim/lua/plugins/multiple-cursors.lua
@@ -1,8 +1,22 @@
 return {
 	"brenton-leighton/multiple-cursors.nvim",
 	version = "*", -- Use the latest tagged version
-	opts = {}, -- This causes the plugin setup function to be called
-	keys = {
+	opts = { -- This causes the plugin setup function to be called
+        custom_key_maps = {
+            -- remap comment keybindings
+            {{"n", "i"}, "<C-/>", function() vim.cmd("normal gcc") end},
+            {"v", "<C-/>", function() vim.cmd("normal gc") end},
+        },
+        pre_hook = function()
+            require("nvim-autopairs").disable()
+            require("cmp").setup({enabled=false})
+        end,
+        post_hook = function()
+            require("nvim-autopairs").enable()
+            require("cmp").setup({enabled=true})
+        end,
+    },
+    keys = {
 		{
 			"<A-j>",
 			"<Cmd>MultipleCursorsAddDown<CR>",
