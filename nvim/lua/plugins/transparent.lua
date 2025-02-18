bfb6ea52180a3437bce04988afbeec184a492585
Cristian Caloian <cristian.caloian@volvocars.com>
2025-02-18 10:12:45 +0100

    edit style changes

--- nvim/lua/plugins/transparent.lua
+++ nvim/lua/plugins/transparent.lua
@@ -1,37 +1,38 @@
 return {
-	"xiyaowong/transparent.nvim",
-	lazy = false, --this was SUPER IMPORTANT
-	config = function()
-		require("transparent").setup({
-			groups = {
-				"Normal",
-				"NormalNC",
-				"Comment",
-				"Constant",
-				"Special",
-				"Identifier",
-				"Statement",
-				"PreProc",
-				"Type",
-				"Underlined",
-				"Todo",
-				"String",
-				"Function",
-				"Conditional",
-				"Repeat",
-				"Operator",
-				"Structure",
-				"LineNr",
-				"NonText",
-				"SignColumn",
-				"CursorLine",
-				"CursorLineNr",
-				"StatusLine",
-				"StatusLineNC",
-				"EndOfBuffer",
-			},
-			extra_groups = { "NeoTreeNormal", "NeoTreeNormalNC", "NormalFloat" },
-			exclude_groups = {},
-		})
-	end,
+    "xiyaowong/transparent.nvim",
+    lazy = false, --this was SUPER IMPORTANT
+    config = function()
+        require("transparent").setup({
+            groups = {
+                "Normal",
+                "NormalNC",
+                "Comment",
+                "Constant",
+                "Special",
+                "Identifier",
+                "Statement",
+                "PreProc",
+                "Type",
+                "Underlined",
+                "Todo",
+                "String",
+                "Function",
+                "Conditional",
+                "Repeat",
+                "Operator",
+                "Structure",
+                "LineNr",
+                "NonText",
+                "SignColumn",
+                "CursorLine",
+                "CursorLineNr",
+                "StatusLine",
+                "StatusLineNC",
+                "EndOfBuffer",
+            },
+            extra_groups = { "NeoTreeNormal", "NeoTreeNormalNC", "NormalFloat" },
+            exclude_groups = {},
+        })
+        require("transparent").clear_prefix("lualine")
+    end,
 }
