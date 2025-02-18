03c3d1267eae2cf6baaa2e169fccd3cc750d3c44
Cristian Caloian <cristian.caloian@volvocars.com>
2025-02-18 10:34:50 +0100

    add lspsaga

---
+++ nvim/lua/plugins/lsp-saga.lua
@@ -0,0 +1,12 @@
+return {
+	"nvimdev/lspsaga.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional
+		"nvim-tree/nvim-web-devicons", -- optional
+	},
+	config = function()
+		require("lspsaga").setup({})
+		vim.keymap.set("n", "<leader>lo", ":Lspsaga outline<CR>", { desc = "[L]spsaga [O]utline" })
+		vim.keymap.set("n", "<leader>lt", ":Lspsaga term_toggle<CR>", { desc = "[L]spsaga [T]erminal" })
+	end,
+}
