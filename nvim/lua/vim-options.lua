12401f80a443b3d3264a20138f914650f3bb09e8
Cristian Caloian <cristian.caloian@volvocars.com>
2025-06-13 14:38:48 +0200

    vim/opt: refactor vim options

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,15 +1,67 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set autoindent")
+-- Tab settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
 
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--  Other symbols: tab:↦\\ ,extends:>,precedes:<,space:·,lead:‹,trail:›
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", lead = "·", trail = "·", nbsp = "␣", eol = "↵" }
+
+-- Set highlight on search
+vim.opt.hlsearch = true
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = { "menuone", "noselect" }
+
+-- NOTE: You should make sure your terminal supports this
+vim.opt.termguicolors = true
+
+-- Global status line
+vim.opt.laststatus = 3
+
+-- Set winbar to show filename
+vim.opt.winbar = "%=%m %f"
+
+-- Global options
 vim.g.mapleader = " "
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
+-- Window options
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- Make line numbers default
+vim.wo.number = true
 
 -- Basic movement keybinds, these make navigating splits easier
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", { desc = "Move to split left" })
@@ -17,48 +69,9 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", { desc = "Move to split down" })
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", { desc = "Move to split up" })
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", { desc = "Move to split right" })
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = "a"
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = "unnamedplus"
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = "yes"
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = "menuone,noselect"
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
--- Global status line
-vim.o.laststatus = 3
-
--- Set winbar to show filename
-vim.o.winbar = "%=%m %f"
+-- Clear search highlighting when pressing <Esc>
+-- alternatively you can run `:let @/ = ""` to clear search
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clear search highlighting" })
 
 -- [[ Basic Keymaps ]]
 
@@ -74,10 +87,9 @@ vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
-
