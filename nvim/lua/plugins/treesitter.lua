bb2b4edd209a1fc6f0ae60de09a64e0eed1189e8
Christian Caloian <christian.caloian@dsv.com>
2026-03-09 13:29:26 +0000

    fix: treesitter breaking change

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,38 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build= ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            -- auto_install = true,
-            ensure_installed = {
-                "bash",
-                "c",
-                "cmake",
-                "cpp",
-                "cuda",
-                "dockerfile",
-                "go",
-                "gomod",
-                "gotmpl",
-                "gowork",
-                "java",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "python",
-                "rust",
-                "scala",
-                "sql",
-                "toml",
-                "typescript",
-                "vim",
-                "yaml",
-            },
-            highlight = { enable = true },
-            indent = { enable = true }
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	enabled = true,
+	config = function()
+		local config = require("nvim-treesitter.config")
+		config.setup({
+			-- auto_install = true,
+			ensure_installed = {
+				"bash",
+				"c",
+				"cmake",
+				"cpp",
+				"cuda",
+				"dockerfile",
+				"go",
+				"gomod",
+				"gotmpl",
+				"gowork",
+				"java",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"python",
+				"rust",
+				"scala",
+				"sql",
+				"toml",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
-
