0b8325db747c49f225ff97f6d31027786bcbd7f9
Christian Caloian <christian.caloian@dsv.com>
2026-06-02 15:10:01 +0200

    nvim/telescope: remove pinned tag 0.1.8

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     -- Telescope
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
+        -- tag = "0.1.8",
         dependencies = "nvim-lua/plenary.nvim",
         config = function()
             local actions = require("telescope.actions")
